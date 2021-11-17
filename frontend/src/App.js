186bed076c75027bdf2d44fe4afd4b2ca57e9bec
Liisa Mikola <70939470+liismik@users.noreply.github.com>
2021-11-17 04:24:26 +0200

    Base files for navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,17 +2,20 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import NavBar from './components/NavBar';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-      </Switch>
-    </BrowserRouter>
-
+    <div>
+      <NavBar />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar(){
+    return(
+        <div>
+            <p>I will be the navbar!!!</p>
+        </div>
+    )
+}
+
+export default NavBar;
