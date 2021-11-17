7968aae816c79676e76e59aef875a6eb14bb1afb
Liisa Mikola <70939470+liismik@users.noreply.github.com>
2021-11-17 04:59:41 +0200

    Logo added, slight css

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './NavBar.css';
+import logo from '../InvestoFoxLogo.svg';
 
 function NavBar(){
     return(
         <div>
-            <p>I will be the navbar!!!</p>
+            <div className="leftBar">
+                <img src={logo} alt="InvestoFox logo" className="logo" />
+                <p>I will be the navbar!</p>
+            </div>
+            <div className="topBar">
+                <p>I will be the top navbar!</p>
+            </div>
         </div>
     )
 }
