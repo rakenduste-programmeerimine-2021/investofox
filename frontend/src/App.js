fdce3e6aed2cc7dca963d1891a40119a9d47ad9f
Liisa Mikola <70939470+liismik@users.noreply.github.com>
2021-11-21 00:59:01 +0200

    Login form component and page creation
    Created the login form component and the login page FE.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path={["/", "/login"]} component={Login} />
         <Route exact path="/register" component={Register} />
       </Switch>
     </BrowserRouter>
---
+++ frontend/src/components/LoginForm.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import './LoginForm.css';
+import { Link } from 'react-router-dom';
+
+function LoginForm() {
+    return(
+        <div className="login-container">
+            <form className="login-content">
+            <div className="login-header">
+                <p className="login-title">Login</p>
+            </div>
+                <div className="login-row">
+                    <label>Email</label>
+                    <input 
+                        type="email" 
+                        name="email" 
+                        placeholder="example@example.com" 
+                        className="inputField" 
+                    />
+                </div>
+                <div className="login-row">
+                    <label>Password</label>
+                    <input 
+                        type="password" 
+                        name="password" 
+                        placeholder="Password" 
+                        className="inputField" 
+                    />
+                </div>
+                <div className="login-footerDiv">
+                    <div className="login-extrasDiv">
+                        <div className="login-rememberBoxDiv">
+                            <input name="rememberme" type="checkbox" />
+                            <label>Remember me</label>
+                        </div>
+                        <Link to="./forgot-password">
+                            <p className="login-forgotPassword">Forgot password?</p>
+                        </Link>
+                    </div>
+                    <div className="login-buttonsDiv">
+                        <Link to="./register">
+                            <button className="login-registerButton">Register</button>
+                        </Link>
+                        <button className="login-loginButton">Login</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default LoginForm;
