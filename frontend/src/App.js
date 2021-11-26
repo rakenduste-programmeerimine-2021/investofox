1b280c31d5b811b6efbbf2c8d2da23f90a2555ac
Liisa Mikola <70939470+liismik@users.noreply.github.com>
2021-11-26 14:04:20 +0200

    Update branch, make navbar appear on every page except login and register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,28 @@ import Register from "./pages/Register";
 import NavBar from './components/NavBar';
 import Login from "./pages/Login";
 
-function App() {
+const authPage = () => (
+  <div>
+    <Route exact path={["/", "/login"]} component={Login} />
+    <Route exact path="/register" component={Register} />
+  </div>
+)
+
+const regularPage = () => (
+  <NavBar/>
+)
+
+const App = () => {
   return (
     <div>
-      <NavBar />
       <BrowserRouter>
         <Switch>
-          <Route exact path={["/", "/login"]} component={Login} />
-          <Route exact path="/register" component={Register} />
+          <Route exact path={["/", "/login", "/register"]} component={authPage}/>
+          <Route component={regularPage}/>
         </Switch>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -3,14 +3,13 @@ import './NavBar.css';
 import logo from '../InvestoFoxLogo.svg';
 
 function NavBar(){
+
+    //let s = "no probs B)";
+
     return(
         <div>
             <div className="leftBar">
                 <img src={logo} alt="InvestoFox logo" className="logo" />
-                <p>I will be the navbar!</p>
-            </div>
-            <div className="topBar">
-                <p>I will be the top navbar!</p>
             </div>
         </div>
     )
