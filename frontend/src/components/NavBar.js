dc317442f716caac2ec90db902b6282068f758ad
Liisa Mikola <70939470+liismik@users.noreply.github.com>
2021-11-26 15:00:17 +0200

    NavBar + design

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,15 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NavBar.css';
 import logo from '../InvestoFoxLogo.svg';
+import './NavBar.css';
 
 function NavBar(){
 
     //let s = "no probs B)";
 
     return(
-        <div>
+        <div className="page">
             <div className="leftBar">
-                <img src={logo} alt="InvestoFox logo" className="logo" />
+                <div className="topHalf">
+                    <img src={logo} alt="InvestoFox logo" className="logo" />
+                    <div className="navBarButtonsDiv">
+                        <Link to="./portfolio">
+                            <button className="navBarButton">Portfolio</button>
+                        </Link>
+                        <Link to="./addOrder">
+                        <button className="navBarButton">Add order</button>
+                        </Link>
+                        <Link to="./order">
+                        <button className="navBarButton">Order</button>
+                        </Link>
+                    </div>
+                </div>
+                <div className="bottomHalf">
+                    <Link to="./settings">
+                        <button className="settingsButton">Settings</button>
+                    </Link>
+                    <p className="message">Made with ♡ by the InvestoFox team</p>
+                </div>
+            </div>
+            <div className="temp">
+                <div className="topBar">
+                </div>
+                <div className="content">
+                </div>
             </div>
         </div>
     )
