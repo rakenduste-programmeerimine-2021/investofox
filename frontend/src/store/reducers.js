6dd81a8575aa298d0c4c25b1b665a2a2749234cc
Gaspar Luik <gasparl@tlu.ee>
2021-12-05 17:53:49 +0200

    User state saved to local storage closes #75

--- frontend/src/store/reducers.js
+++ frontend/src/store/reducers.js
@@ -1,15 +1,17 @@
 /* Reducer takes in an action defined in actions.js. Then handles the state of an app using the action*/
 
-import { USER_UPDATE, USER_REMOVE, USER_LOGIN, USER_LOGOUT } from "./actions";
+import {USER_LOGIN, USER_LOGOUT } from "./actions";
 
 const userReducer = (state, action) => {
     switch(action.type){
+        //sets global state if the user to be logged in
         case USER_LOGIN:
             return{
                 ...state,
                 token: action.payload.token,
-                user: action.payload.user
+                user: action.payload.id
             }
+        //sets the user's global state to be null aka logged out
         case USER_LOGOUT:
             return{
                 ...state,
