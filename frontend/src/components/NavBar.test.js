50f7c4c8535162d520833073b77d762d05390b92
Gaspar Luik <luik.gaspar@gmail.com>
2021-12-16 15:15:36 +0200

    enzyme dependency

--- frontend/src/components/NavBar.test.js
+++ frontend/src/components/NavBar.test.js
@@ -2,6 +2,10 @@ import React from 'react'
 import NavBar from './NavBar'
 import { getByTestId, getByText, render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
+import Enzyme, {shallow} from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+
+Enzyme.configure({ adapter: new Adapter()})
 
 describe("NavBar", () => {
   describe("Renders the Navbar component", () => {
@@ -10,13 +14,21 @@ describe("NavBar", () => {
     });
 
     it("Checks if buttons render", () => {
-        const {getByTestId, getByText} = render(<BrowserRouter><NavBar /></BrowserRouter>)
+        const {getByTestId} = render(<BrowserRouter><NavBar /></BrowserRouter>)
         getByTestId("orders-test")
         getByTestId("stock-fetch-test")
         getByTestId("add-order-test")
         getByTestId("chart-test")
         getByTestId("settings-test")
-
     });
   })
+
+  describe('Test if button redirects', () => {
+    it('Test click event', () => {
+
+      const wrapper = shallow(<BrowserRouter><NavBar /></BrowserRouter>)
+      const button = wrapper.find('settings-test').simulate('click')
+      expect(button).toBe(1)
+    });
+  });
 })
