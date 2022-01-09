a7d02037de1faa84cb15d9584def055186522179
gaspurr <luik.gaspar@gmail.com>
2022-01-09 20:15:08 +0200

    some changes

--- backend-node/src/server.js
+++ backend-node/src/server.js
@@ -6,7 +6,6 @@ require("dotenv").config()
 const cors = require('cors');
 const authRoutes = require('./routes/auth')
 const orderRoutes = require('./routes/order')
-const axios = require('axios')
 
 const corsOptions = {
   origin: 'http://localhost:3000',
@@ -23,56 +22,9 @@ app.use('/api/auth', authRoutes)
 app.use('/portfolio', jwtAuth)
 app.use('/api/order', orderRoutes)
 
-app.get('/secret', jwtAuth, (req, res) => {
-  res.send('Secret Hello World!')
-})
-//fetch testing
-app.use('/stock', jwtAuth, function(req, res, next) {
-  const apikey = "M7DSRJECMBCEEWGF";
-  const ticker = ['MSFT']
-
-  let completed = 0;
-  const results = [];
-  console.log(ticker);
-  for (let i = 0; i < ticker.length; i++) {
-    const oneTicker = ticker[i];
-
-    axios.get(
-        `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&apikey=${apikey}`
-      )
-    .then((response) => {
-      completed += 1;
-
-      results.push(response.data);
-      if (completed === ticker.length) {
-        //All ticker have finished their response
-        console.log("completed");
-
-        res.send({
-          success: true,
-          message: "Ticker info",
-          results,
-        });
-      }
-
-      console.log(ticker);
-    })
-    .catch((e) => {
-      console.error(e);
-    });
-  }
-})
-
-app.get('/auth', jwtAuth, (req, res) => {
-  res.status(200).send("Welcome home, cowboy")
-})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-})
-
-app.get('/secret', jwtAuth, (req, res) => {
-  res.send('Secret Hello World!')
 })
 
 app.get('*', (req, res) => {
@@ -84,8 +36,7 @@ if(process.env.NODE_ENV == 'production') {
   app.use(express.static('frontend/build'))
 }
 
-mongoose
-  .connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://root:root@mongo/investofox?authSource=admin', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
